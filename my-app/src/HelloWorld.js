6eb9fb45687115b3efc25752bf1d3cf4422037d4
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-07-11 17:00:17 +0200

    svolgimento esercizio 5

--- my-app/src/HelloWorld.js
+++ my-app/src/HelloWorld.js
@@ -1,16 +1,13 @@
 import React from 'react'
 
-const nome = <h1>Paolo</h1>;
-
 export default class Helloworld extends React.Component{
-    hello(name){
-        return 'Hello, '+ name; 
+
+    combineParameters(a,b){
+        return a + b
     }
 
-
     render(){
-       return <h1>{this.hello(nome)}</h1>
+       return <h2>{this.combineParameters(45,62)}</h2>
     }
 }
 
-//if the name variable contains a JSX expression instead of a string the result is "Hello, [Object, Object]"
